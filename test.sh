6d4b12e6d633d4bc2e8f02cf9454dd9bc439d7e2
Michael Reed <mreed@gilt.com>
2014-12-28 13:00:56 -0500

    first commit

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+cd $(dirname $0)
+PATH="$(pwd)/bin:$PATH"
+
+VAULT_TEST="$HOME/vault-test"
+if [ ! -d $VAULT_TEST  ]; then
+  mkdir -p $VAULT_TEST
+  cd $VAULT_TEST
+  git init --bare
+fi
+
+WORKSPACE="$(mktemp -d -t "vault-test-workspace")"
+cd $WORKSPACE
+
+rm -rf $HOME/.vault
+vault init file://$VAULT_TEST
+rm -rf $HOME/.vault
+vault clone file://$VAULT_TEST
+cd vault-test
+vault fetch
+vault pull
+echo 'Hello, World!' > test
+git add test
+git commit -m "testing"
+vault push
